9101b9eb1626ae65c925e4a78c55713d19d74270
wfma <mwf1998@qq.com>
2023-05-02 09:51:33 +0800

    add by mawf at 2023/05/02

---
+++ 14_webpack-vue/src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue';
+import { sum } from "./js/math";
+const { priceFormat } = require("./js/format");
+
+import App from './vue/App.vue';
+
+import "./js/element";
+
+console.log(sum(20, 30));
+console.log(priceFormat());
+
+
+const app = createApp(App);
+app.mount("#app");
